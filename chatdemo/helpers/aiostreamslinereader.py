614e86b770c572f305ea03bbfe789d7ac1685ea1
liu_xue_yan <liu_xue_yan@foxmail.com>
2019-12-10 19:04:53 +0800

    remove: unused paramenter of aiostreamlinereader

--- chatdemo/helpers/aiostreamslinereader.py
+++ chatdemo/helpers/aiostreamslinereader.py
@@ -4,22 +4,17 @@ from typing import List, Optional
 
 
 class AioStreamsLineReader:
-
     EOF = -1
 
     def __init__(self,
                  streams: List[asyncio.StreamReader] = [],
                  encoding: Optional[str] = None,
-                 raise_on_eof: bool = False,
-                 raise_on_timeout: bool = False,
                  timeout: float = 1
                  ):
         if not streams:
             raise ValueError('streams cannot be empty.')
         self._streams = streams
         self._encoding = encoding
-        self._raise_on_eof = bool(raise_on_eof)
-        self._raise_on_timeout = bool(raise_on_timeout)
         self._timeout = float(timeout)
         self._at_eof = False
         self._exit_flag = False
@@ -31,30 +26,24 @@ class AioStreamsLineReader:
             line = await stream.readline()
             return line, tag
 
+        encoding = self._encoding or getpreferredencoding()
         lines = [None for _ in range(len(self._streams))]
         aws = {
             asyncio.create_task(_read(stream, i))
             for i, stream in enumerate(self._streams)
         }
-        try:
-            done, pending = await asyncio.wait(aws, timeout=self._timeout, return_when=asyncio.FIRST_COMPLETED)
-        except asyncio.TimeoutError:
-            if self._raise_on_timeout:
-                raise
-        else:
-            for task in pending:
-                task.cancel()
-            for task in done:
-                output_bytes, index = task.result()
-                if output_bytes == b'':  # EOF!
-                    self._at_eof = True
-                    if self._raise_on_eof:
-                        raise EOFError()
-                    lines[index] = self.EOF
-                else:
-                    line = output_bytes.decode(
-                        self._encoding or getpreferredencoding()).strip()
-                    lines[index] = line
+        done, pending = await asyncio.wait(aws, timeout=self._timeout, return_when=asyncio.FIRST_COMPLETED)
+        for task in pending:
+            task.cancel()
+        for task in done:
+            output_bytes, index = task.result()
+            if output_bytes == b'':  # EOF!
+                self._at_eof = True
+                lines[index] = self.EOF
+            else:
+                self._at_eof = False
+                line = output_bytes.decode(encoding).strip()
+                lines[index] = line
         return tuple(lines)
 
     async def __aenter__(self):
@@ -73,24 +62,13 @@ class AioStreamsLineReader:
         if exit_flag:
             raise GeneratorExit()
         lines = await self.readline()
-        if self.is_eof(lines):
+        if self._at_eof:
             raise StopAsyncIteration()
         return lines
-
-    def is_eof(self, lines):
-        return any(line == self.EOF for line in lines)
 
     @property
     def encoding(self):
         return self._encoding
-
-    @property
-    def raise_on_eof(self):
-        return self._raise_on_eof
-
-    @property
-    def raise_on_timeout(self):
-        return self._raise_on_timeout
 
     @property
     def timeout(self):
