cfbd3210a6cabb9c6453517ed356879b4996acb5
liu_xue_yan <liu_xue_yan@foxmail.com>
2019-12-19 18:35:39 +0800

    edit: scripts

--- scripts/chat_345m.sh
+++ scripts/chat_345m.sh
@@ -4,4 +4,4 @@
     --allow-origins "*" \
     --chat-prog /home/kangzh/miniconda3/envs/python3.6/bin/python \
     --chat-pwd /home/kangzh/transfer-learning-conv-ai \
-    --chat-args "interact2.py --model_checkpoint model_checkpoint_345_32k_v3 --dataset_cache xinli001_jiandanxinli-qa.topics-convai-GPT2BPETokenizer_CN_32K_BPE-cache/cache --min_length 16 --max_length 256"
+    --chat-args "interact2.py --min_length 16 --max_length 256 --model_checkpoint model_checkpoint_345_32k_v7 --dataset_cache xinli001_jiandanxinli-qa.topics-convai-GPT2BPETokenizer_CN_32K_BPE-cache_v12.1/cache"
