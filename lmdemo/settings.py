75ac5884656f3b661af5810582957b7e2760b3df
liu_xue_yan <liu_xue_yan@foxmail.com>
2019-12-31 10:13:21 +0800

    adjust: settings

--- lmdemo/settings.py
+++ lmdemo/settings.py
@@ -1,27 +1,29 @@
+import logging
 from os import getcwd
+from pprint import pformat
 from sys import executable
 
 from pydantic import BaseSettings, Field
 
-ENV_PREFIX = 'APP'
+ENV_PREFIX = 'WEBAPP'
 
 
-def env(s: str) -> str:
-    return '{0}_{1}'.format(
-        ENV_PREFIX,
-        s.strip().strip('_').upper()
-    )
+def e(name: str) -> str:
+    return '_'.join((ENV_PREFIX, name.strip().strip('_').upper()))
 
 
 class Settings(BaseSettings):
-    allow_origins: str = Field('*', env=env('allow_origins'))
+    allow_origins: str = Field('*', env=e('allow_origins'))
 
-    chat_program: str = Field(executable, env=env('chat_program'))
-    chat_args: str = Field('', env=env('chat_args'))
-    chat_cwd: str = Field(getcwd(), env=env('chat_cwd'))
+    chat_program: str = Field(executable, env=e('chat_program'))
+    chat_args: str = Field('', env=e('chat_args'))
+    chat_cwd: str = Field(getcwd(), env=e('chat_cwd'))
 
-    qa_program: str = Field(executable, env=env('qa_program'))
-    qa_args: str = Field('', env=env('qa_args'))
-    qa_cwd: str = Field(getcwd(), env=env('qa_cwd'))
+    qa_program: str = Field(executable, env=e('qa_program'))
+    qa_args: str = Field('', env=e('qa_args'))
+    qa_cwd: str = Field(getcwd(), env=e('qa_cwd'))
+
 
 settings = Settings()
+
+logging.getLogger(__name__).info('settings:\n%s', pformat(settings.dict()))
